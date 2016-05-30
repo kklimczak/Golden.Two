4ae223b62ef918b6fab22094a2a281ee9c5d7a39
Konrad Klimczak <konrad.klimczak@comarch.com>
2016-05-30 09:47:31 +0200

    change of log file to actual date

--- pk/projekt/src/com/goldentwo/utils/Logger/Logger.java
+++ pk/projekt/src/com/goldentwo/utils/Logger/Logger.java
@@ -17,11 +17,12 @@ public class Logger {
 	public void info(String message) {
 		Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        SimpleDateFormat sdfDay = new SimpleDateFormat("yyyy-MM-dd");
 		System.out.printf("%s [%-20.20s] INFO | %s%n", sdf.format(cal.getTime()), classValue.getSimpleName()+".java", message);
 		String log = String.format("%s [%-20.20s] INFO | %s%n", sdf.format(cal.getTime()), classValue.getSimpleName()+".java", message);
 		
 		try{
-			FileOutputStream file = new FileOutputStream("logi.txt", true);
+			FileOutputStream file = new FileOutputStream(sdfDay.format(cal.getTime()) + ".log", true);
 			file.write(log.getBytes());
 			file.close();
 		} catch (IOException exception) {
