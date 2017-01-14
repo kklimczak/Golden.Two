1cc68346ae573225c831f98a27ba7f2904e46833
kklimczak <konrad.klimczak.4@gmail.com>
2017-01-14 16:21:49 +0100

    add multer conf

--- pck/zad5/app.js
+++ pck/zad5/app.js
@@ -1,13 +1,19 @@
 let express = require('express');
+let multer = require('multer');
+
+let MULTER_CONF = {
+    dest: 'files/'
+};
 
 let app = express();
+let upload = multer(MULTER_CONF);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/file/upload', (req, res) => {
-
+app.post('/file/upload', upload.single('xml'), (req, res, next) => {
+    res.sendStatus(200);
 });
 
 app.listen(3000, () => {
