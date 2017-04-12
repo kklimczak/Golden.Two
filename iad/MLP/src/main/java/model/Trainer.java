c49b8d881a329513baa5dab70c332792fbe9f5d8
lukuta <kutalukasz@gmail.com>
2017-04-12 13:14:10 +0200

    added msu error graph

--- iad/MLP/src/main/java/model/Trainer.java
+++ iad/MLP/src/main/java/model/Trainer.java
@@ -1,7 +1,10 @@
 package model;
 
 import lombok.Data;
+import org.jfree.data.xy.XYDataItem;
+import utils.GraphUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -16,16 +19,21 @@ public class Trainer {
     private double eps = 0.01;
     private double momentum = 0.5;
 
+    private GraphUtil graphUtil;
+
     public Trainer(MLP mlp, List<List<Double>> inputs, List<List<Double>> expectedOutputs) {
         this.mlp = mlp;
         this.inputs = inputs;
         this.expectedOutputs = expectedOutputs;
+        this.graphUtil = new GraphUtil();
     }
 
     public void train() {
 
         mlp.setLearningRate(learningRate);
         mlp.setMomentum(momentum);
+
+        List<XYDataItem> dataItems = new ArrayList<>();
 
         int epoch = 0;
         double cost;
@@ -42,11 +50,15 @@ public class Trainer {
                 cost += calculateCost(result, expectedOutputs.get(i));
             }
 
+            dataItems.add(new XYDataItem(epoch, cost));
+
         } while (epoch < maxIterations && cost > eps);
 
         System.out.println("Trening summary: ");
         System.out.println("Iterations: " + epoch);
         System.out.println("Cost: " + cost);
+
+        graphUtil.printGraph("MSU errors", dataItems, "epoch", "MSU");
     }
 
     private double calculateCost(List<Double> result, List<Double> expectedOutput) {
--- iad/MLP/src/main/java/utils/GraphUtil.java
+++ iad/MLP/src/main/java/utils/GraphUtil.java
@@ -4,38 +4,37 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
+import java.util.List;
+
 public class GraphUtil {
 
-    public void printGraph(String title){
-        final Graph demo = new Graph(title);
+    public void printGraph(String title, List<XYDataItem> data, String xLabel, String yLabel){
+        final Graph demo = new Graph(title, data, xLabel, yLabel);
         demo.pack();
         RefineryUtilities.centerFrameOnScreen(demo);
         demo.setVisible(true);
     }
 
     private class Graph extends ApplicationFrame{
-        Graph(String title) {
+        Graph(String title, List<XYDataItem> dataItems, String xLabel, String yLabel) {
             super(title);
-            final XYSeries series = new XYSeries("Random Data");
-            series.add(1.0, 500.2);
-            series.add(5.0, 694.1);
-            series.add(4.0, 100.0);
-            series.add(12.5, 734.4);
-            series.add(17.3, 453.2);
-            series.add(21.2, 500.2);
-            series.add(21.9, null);
-            series.add(25.6, 734.4);
-            series.add(30.0, 453.2);
+            final XYSeries series = new XYSeries(title);
+
+            for (XYDataItem dataItem : dataItems) {
+                series.add(dataItem);
+            }
+
             final XYSeriesCollection data = new XYSeriesCollection(series);
             final JFreeChart chart = ChartFactory.createXYLineChart(
-                    "XY Series Demo",
-                    "X",
-                    "Y",
+                    title,
+                    xLabel,
+                    yLabel,
                     data,
                     PlotOrientation.VERTICAL,
                     true,
