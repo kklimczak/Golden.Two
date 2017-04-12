e2ff3f2013c65cad39df2800eb17f4134bc822a2
lukuta <kutalukasz@gmail.com>
2017-04-12 10:52:27 +0200

    small refactor

--- iad/MLP/src/main/java/utils/ActivationFunc.java
+++ iad/MLP/src/main/java/utils/ActivationFunc.java
@@ -3,11 +3,11 @@ package utils;
 import static java.lang.Math.exp;
 
 public class ActivationFunc {
-    public static Double sigmoid(Double value){
-            return 1.0/(1.0+exp(-value));
+    public static Double sigmoid(Double value) {
+        return 1.0 / (1.0 + exp(-value));
     }
 
-    public static Double sigmoidDerivative(Double value){
-        return exp(value)/((exp(value) + 1) * (exp(value) + 1));
+    public static Double sigmoidDerivative(Double value) {
+        return exp(value) / ((exp(value) + 1) * (exp(value) + 1));
     }
 }
